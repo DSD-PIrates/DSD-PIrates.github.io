aed11afb1391bf5162bfe4edebc19660fb69c148
atoposyz <76671919+atoposyz@users.noreply.github.com>
2023-03-10 22:29:48 +0800

    test update

--- js/index.js
+++ js/index.js
@@ -5,11 +5,19 @@ var menulist = [
     ["借用"],
     ["在线数据"]
 ]
+var linklist = [
+    ["#"],
+    ["docs/test.html", "#", "#", "#", "#", "#"],
+    ["#"],
+    ["#"],
+    ["#"]
+]
 
 var caret = '<span class="caret"></span>'
 var presentation_normal = '<li role="presentation">'
 var presentation_dropdown = '<li role="presentation" class="dropdown">'
-var dropdown_toggle = '<a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">'
+var dropdown_toggle_1 = '<a class="dropdown-toggle" data-toggle="dropdown" href="'
+var dropdown_toggle_2 =  '" role="button" aria-haspopup="true" aria-expanded="false">'
 var dropdown_menu = '<ul class="dropdown-menu">'
 var toptext = `
 <p style="font-size: x-large; text-align: center; margin: 0; padding-left: 10px; padding-right: 10px;">小组名-项目名</p>
@@ -20,10 +28,10 @@ window.onload = function(){
     for(var i = 0; i < menulist.length; i++) {
         var rlength = menulist[i].length
         if(rlength == 1) {
-            text = text + presentation_normal + '<a href="#">' + menulist[i][0] + '</a></li>'
+            text = text + presentation_normal + '<a href="' + linklist[i][0] + '">' + menulist[i][0] + '</a></li>'
         }
         else {
-            text = text + presentation_dropdown + dropdown_toggle + menulist[i][0] + caret + '</a>' + dropdown_menu
+            text = text + presentation_dropdown + dropdown_toggle_1 + linklist[i][0] + dropdown_toggle_2 + menulist[i][0] + caret + '</a>' + dropdown_menu
             for(var j = 1; j < rlength; j++) {
                 text = text + "<li>" + menulist[i][j] + "</li>"
             }
