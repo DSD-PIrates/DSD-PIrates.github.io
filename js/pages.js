c99aeca1a8b012129449f7305ce8c617de3a87a3
atoposyz <76671919+atoposyz@users.noreply.github.com>
2023-03-13 23:18:16 +0800

    更新 pages.js

--- js/pages.js
+++ js/pages.js
@@ -15,7 +15,7 @@ function loadList(num) {
         List = TmpList[num]
         console.log(List)
         for(var i = 0; i < List.length; i++) {
-            text = text + `<div class="day"><div class="postTitle"><a href="` + List[i].src + `">` + List[i].title + `</a></div><span class="postMeta">Posted on ` + List[i].time + `</span><div class="postCon"><div class="mainText">` + List[i].abstract + `</div><a herf="` + List[i].src + `">Read more.</a></div>`
+            text = text + `<div class="day"><div class="postTitle"><a href="` + List[i].src + `">` + List[i].title + `</a></div><span class="postMeta">Posted on ` + List[i].time + `</span><div class="postCon"><div class="mainText">` + List[i].abstract + `</div><a href="` + List[i].src + `">Read more.</a></div>`
         }
         console.log(text)
         document.getElementById("articlelist").innerHTML = text
