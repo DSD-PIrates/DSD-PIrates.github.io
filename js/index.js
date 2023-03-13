cbd3ad3e3632ebc4c2c13a1ecd5082610682f379
atoposyz <76671919+atoposyz@users.noreply.github.com>
2023-03-13 13:47:59 +0800

    removed

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var menulist = [
     [{name: "Home", src: "index.html"}],
     [{name: "NEWS", src: ""}, {name: "Week1", src: "docs/week1.html"}, {name: "More", src: "docs/news.html"}],
-    [{name: "Documents", src: ""}, {name: "FirstEmailFromUTAD", src: "docs/FirstEmailFromUTAD.html"}, {name: "GDSS", src: "docs/GDSS.html"}, {name: "More", src :""}],
+    [{name: "Documents", src: ""}, {name: "GDSS", src: "docs/GDSS.html"}, {name: "More", src :""}],
     [{name: "Members", src: "docs/member.html"}],
     [{name: "Things", src: "docs/borrow.html"}],
     [{name: "Online", src: "docs/realtimedata.html"}]
