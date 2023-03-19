45d610b26811eda44d96462c4c5d27e9e9489162
atoposyz <76671919+atoposyz@users.noreply.github.com>
2023-03-19 13:10:53 +0800

    update title2json

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 var menulist = [
     [{name: "Home", src: "index.html"}],
-    [{name: "NEWS", src: ""}, {name: "BlogForWeek1", src: "docs/BlogForWeek1.html"}, {name: "More", src: "docs/news.html"}],
-    [{name: "Documents", src: ""}, {name: "Research on end-to-end latency between different devices", src: "docs/Research on end-to-end latency between different devices.html"}, {name: "GDSS", src: "docs/GDSS.html"}, {name: "More", src :"docs/documents.html"}],
+    [
+        {name: "NEWS", src: ""}, 
+        {name: "BlogForWeek1", src: "docs/BlogForWeek1.html"}, 
+        {name: "More", src: "docs/news.html"}
+    ],[
+        {name: "Documents", src: ""}, 
+        {name: "Research on end-to-end latency between different devices", src: "docs/Research on end-to-end latency between different devices.html"}, 
+        {name: "GDSS", src: "docs/GDSS.html"}, {name: "More", src :"docs/documents.html"}
+    ],
     [{name: "Members", src: "docs/member.html"}],
     [{name: "Things", src: "docs/borrow.html"}],
     [{name: "Online", src: "docs/realtimedata.html"}]
@@ -50,46 +57,5 @@ function topbar(flag) {
     }
     text = text + `</ul></ul></div>`
     console.log(text)
-    //document.getElementById("top").innerHTML = text
     document.getElementById("top").innerHTML = toptext + text
 }
-/*
-var caret = '<span class="caret"></span>'
-var presentation_normal = '<li role="presentation">'
-var presentation_dropdown = '<li role="presentation" class="dropdown">'
-var dropdown_toggle_1 = '<a class="dropdown-toggle" data-toggle="dropdown" href="'
-var dropdown_toggle_2 =  '" role="button" aria-haspopup="true" aria-expanded="false">'
-var dropdown_menu = '<ul class="dropdown-menu">'
-var toptext = `
-    <p style="font-size: x-large; text-align: center; margin: 0; padding-left: 10px; padding-right: 10px;">小组名-项目名</p>
-    <ul class="nav nav-pills" id="toptable">`
-
-function isNormalPage(flag) {
-    if(flag == 0) {
-        return ""
-    } else {
-        return "../"
-    }
-}
-
-function topbar(flag) {
-    var text = ""
-    for(var i = 0; i < menulist.length; i++) {
-        var rlength = menulist[i].length
-        if(rlength == 1) {
-            text = text + presentation_normal + '<a href="' + isNormalPage(flag) + menulist[i][0].src + '">' + menulist[i][0].name + '</a></li>'
-        }
-        else {
-            text = text + presentation_dropdown + dropdown_toggle_1 + isNormalPage(flag) + menulist[i][0].src + dropdown_toggle_2 + menulist[i][0].name + caret + '</a>' + dropdown_menu
-            for(var j = 1; j < rlength; j++) {
-                text = text + '<li><a href="' + isNormalPage(flag) + menulist[i][j].src + '">' + menulist[i][j].name + "</a></li>"
-            }
-            text = text + "</ul></li>"
-        }
-        
-    }
-    text = text + '</ul>'
-    console.log(text)
-    //document.getElementById("top").innerHTML = text
-    document.getElementById("top").innerHTML = toptext + text
-}*/
