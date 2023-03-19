c25e0e3065dbc56185763d25fea8879ae3abe4bc
atoposyz <76671919+atoposyz@users.noreply.github.com>
2023-03-19 11:39:58 +0800

    bootstrap3 to bootstrap5
    important!

--- js/index.js
+++ js/index.js
@@ -8,26 +8,21 @@ var menulist = [
 ]
 
 var caret = '<span class="caret"></span>'
-var presentation_normal = '<li role="presentation">'
-var presentation_dropdown = '<li role="presentation" class="dropdown">'
-var dropdown_toggle_1 = '<a class="dropdown-toggle" data-toggle="dropdown" href="'
-var dropdown_toggle_2 =  '" role="button" aria-haspopup="true" aria-expanded="false">'
-var dropdown_menu = '<ul class="dropdown-menu">'
+var presentation_normal = '<li class="nav-item">'
+var presentation_dropdown = '<li class="nav-item dropdown">'
+var dropdown_toggle_1 = '<a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="'
+var dropdown_toggle_2 =  '" role="button" aria-expanded="false">'
+var dropdown_menu = '<ul class="dropdown-menu" aria-labelledby="navbarDropdown">'
 var toptext = `
-<div class="container-fluid">
-<div class="navbar-header">
-    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-        data-target="#collapsed-nav">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
+<div class="navbar-brand">PIrates-RoseberryPi
+    <button type="button" class="navbar-toggler" data-bs-toggle="collapse"
+        data-bs-target="#collapsed-nav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
     </button>
-    <div class='navbar-brand'>
-        PIrates-RoseberryPi
-    </div>
+    
 </div>
 <div class="collapse navbar-collapse" id="collapsed-nav">
-    <ul class="nav navbar-nav">`
+    <ul class="navbar-nav">`
 
 function isNormalPage(flag) {
     if(flag == 0) {
@@ -42,18 +37,18 @@ function topbar(flag) {
     for(var i = 0; i < menulist.length; i++) {
         var rlength = menulist[i].length
         if(rlength == 1) {
-            text = text + presentation_normal + '<a href="' + isNormalPage(flag) + menulist[i][0].src + '">' + menulist[i][0].name + '</a></li>'
+            text = text + presentation_normal + '<a class="nav-link" href="' + isNormalPage(flag) + menulist[i][0].src + '">' + menulist[i][0].name + '</a></li>'
         }
         else {
             text = text + presentation_dropdown + dropdown_toggle_1 + isNormalPage(flag) + menulist[i][0].src + dropdown_toggle_2 + menulist[i][0].name + caret + '</a>' + dropdown_menu
             for(var j = 1; j < rlength; j++) {
-                text = text + '<li><a href="' + isNormalPage(flag) + menulist[i][j].src + '">' + menulist[i][j].name + "</a></li>"
+                text = text + '<li><a class="dropdown-item" href="' + isNormalPage(flag) + menulist[i][j].src + '">' + menulist[i][j].name + "</a></li>"
             }
             text = text + "</ul></li>"
         }
         
     }
-    text = text + `</ul></ul></div></div>`
+    text = text + `</ul></ul></div>`
     console.log(text)
     //document.getElementById("top").innerHTML = text
     document.getElementById("top").innerHTML = toptext + text
